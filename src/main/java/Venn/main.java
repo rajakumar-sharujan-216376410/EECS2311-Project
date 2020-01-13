d8c1ddcd8eec314a3146964970cf078a56d2008d
ahmedhagi <ahmedhagi1998@gmail.com>
2020-01-13 13:31:24 -0500

    method test

--- src/main/java/Venn/main.java
+++ src/main/java/Venn/main.java
@@ -1,9 +1,19 @@
 package Venn;
 
 public class main {
+	
+	public static void firstPrint(){
+		System.out.println("Hey");
+	} 
+	
+	public static void secondPrint(){
+		System.out.println("Hi");
+	} 
 
 	public static void main(String[] args) {
 		System.out.println("Hello ");//ff
+		firstPrint();
+		secondPrint();
 	
 
 	}
