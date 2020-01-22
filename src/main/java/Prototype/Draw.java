1c0b01443d749b556501942d360d0b40debc0c5d
Sharujan Rajakumar <sharujan@my.yorku.ca>
2020-01-22 12:23:59 -0500

    made a input dialog so the user can enter the dimensions of the circle and re-size it depending on their interest

--- src/main/java/Prototype/Draw.java
+++ src/main/java/Prototype/Draw.java
@@ -2,21 +2,33 @@ package Prototype;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.Scanner;
 
-public class Draw extends JPanel{
+public class Draw extends JPanel {
 	
-	public void drawing(){
+	private int xPos, yPos, distance;
+	private String user, user2, user3;
+	
+	public void drawing() {
 		//Draws the two circles
 		repaint();
 	}
-	
-	public void paintComponent(Graphics g){
+
+	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		//Sets the settings for the Venn Diagram
+		
+		user = JOptionPane.showInputDialog(this, "Enter the x position of the circle: ");
+		user2 = JOptionPane.showInputDialog(this, "Enter the y position of the circle: ");
+		user3 = JOptionPane.showInputDialog(this, "Enter the separation distance of the circles: ");
+		
+		xPos = Integer.parseInt(user);
+		yPos = Integer.parseInt(user2);
+		distance = Integer.parseInt(user3);
+		
 		g.setColor(Color.RED);
-		g.drawOval(50, 100, 250, 250);
+		g.fillOval(50, 100, xPos, yPos);
 		g.setColor(Color.GREEN);
-		g.drawOval(175, 100, 250, 250);
+		g.fillOval(distance, 100, xPos, yPos);
 	}
-
 }
--- src/main/java/Prototype/MainFrame.java
+++ src/main/java/Prototype/MainFrame.java
@@ -1,10 +1,19 @@
 package Prototype;
 
 import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.Label;
+
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+
 import java.awt.event.*;
 
 //Creates the frame for the application
@@ -13,12 +22,13 @@ public class MainFrame extends JFrame implements ActionListener {
 	private JPanel panel;
 	private JButton show;
 	private JButton hide;
+	private JButton newCircle;
 	private Draw object;
 	
-	public MainFrame(){
+	public MainFrame() {
 		//Creates empty JFrame window with the name 'Venn App'
 		super("Venn App");
-		
+
 		//Sets the Layout to Border Layout style
 		setLayout(new BorderLayout());
 		
@@ -29,10 +39,12 @@ public class MainFrame extends JFrame implements ActionListener {
 		//Creates show & hide buttons
 		show = new JButton("Show");
 		hide = new JButton("Hide");
+		newCircle = new JButton("new Circle");
 		
 		//Adds the button click Listener
 		show.addActionListener(this);
 		hide.addActionListener(this);
+		newCircle.addActionListener(this); 
 
 		//Sets the settings for the window 
 		setSize(600, 500);
@@ -45,29 +57,41 @@ public class MainFrame extends JFrame implements ActionListener {
 		//adds the Components 
 		panel.add(show);
 		panel.add(hide);
+		panel.add(newCircle);
 		add(panel, BorderLayout.WEST);
 		add(object, BorderLayout.CENTER);
 		
 		//Draws the two circles and hides the Venn diagram
 		object.drawing();
 		object.setVisible(false);
+		
+		JTextField text = new JTextField(6);
+		text.setText("Similarities");
+		text.moveCaretPosition(10); 
+		object.add(text);
+		
+		Menu menu1 = new Menu();
+		panel.add(menu1);
 	}
 	
 	//This method is run once a button is clicked
-	public void actionPerformed (ActionEvent e){
+	public void actionPerformed (ActionEvent e) {
 		//Action for Show Button
 		if (e.getSource() == show){
 			
 			//Shows the drawing
 			object.setVisible(true);
+
 		}
 		//Action for Hide button
-		else if (e.getSource() == hide){
+		else if (e.getSource() == hide) {
 			
 			//Hides the drawing 
 			object.setVisible(false);
 		}
+		else if (e.getSource() == newCircle) {
+			
+			object.drawing();
+		}
 	}
-	
-	
 }
