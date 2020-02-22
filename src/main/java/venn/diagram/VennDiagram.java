ab4cf5390a92d1b27ec65645aabb323efecd509e
Hasan Al-Habbobi <hasan22@my.yorku.ca>
2020-02-22 17:38:02 -0500

    Added missing code to launch file

--- src/main/java/venn/diagram/VennDiagram.java
+++ src/main/java/venn/diagram/VennDiagram.java
@@ -1,5 +1,29 @@
 package venn.diagram;
 
-public class VennDiagram {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+
+public class VennDiagram extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("mainFXML.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
 }
