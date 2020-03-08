a6687783ae5af3a844cd6c762aa7395e5cef37d0
ahmedhagi <ahmedhagi1998@gmail.com>
2020-03-07 19:23:47 -0500

    Updated the Venn Diagram

--- src/main/java/venn/diagram/VennDiagram.java
+++ src/main/java/venn/diagram/VennDiagram.java
@@ -1,21 +1,29 @@
 package venn.diagram;
 
+import java.util.Properties;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 
 public class VennDiagram extends Application {
     
+    //initiate the persistent class
+    Persistent persisten = new Persistent();
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("mainFXML.fxml"));
         
         Scene scene = new Scene(root);
         
+        stage.getIcons().add(new Image(VennDiagram.class.getResourceAsStream("icon.png")));
+        stage.setTitle("VennGram");
         stage.setScene(scene);
+        stage.setResizable(false);
+        stage.setMaximized(true);
         stage.show();
     }
 
